bbe222691bcbc875cff58748cabfe7bcc46cb9e6
Jay <danielameh048@gmail.com>
2022-06-30 11:44:13 -0700

    Hottest fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 /**
  * main -Entry point
  * Description: 'A new project abot using puts with special symbols'
@@ -7,9 +8,6 @@
 */
 int main(void)
 {
-	char ch[59];
-
-	strcpy(ch, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	putc(ch, stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
