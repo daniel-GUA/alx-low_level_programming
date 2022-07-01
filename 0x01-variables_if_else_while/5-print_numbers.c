6fcd71ddd40ecb2555da947fa900fb3a008fc83f
Jay <danielameh048@gmail.com>
2022-07-01 13:38:05 -0700

    Hot fix

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- Entry point
+ * Description:C file that reads alphabet
+ * Return:Always 0 (success)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
